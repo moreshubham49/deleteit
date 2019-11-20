60d914e061f778667e5a7826d7503fa15c42c6a4
shubham More <shubham@more>
2019-11-20 20:07:22 +0530

    first commit

---
+++ tp/queries.sql
@@ -0,0 +1,10 @@
+CREATE TABLE emp(id integer primary key auto_increment,name varchar);
+
+insert into emp(name) values ('emp1');
+insert into emp(name) values ('emp2');
+insert into emp(name) values ('emp3');
+insert into emp(name) values ('emp4');
+insert into emp(name) values ('emp5');
+
+
+
